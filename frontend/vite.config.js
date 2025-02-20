7a2ffec7dec8bc300d1d48f781ea9ae2e6e32bb5
Atanas Velev <atanas.velev@nag.ch>
2025-02-20 16:29:29 +0200

    changing env variable access so react frontend can getbase url from Divio env variables pool

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   
   return {
     plugins: [react()],
@@ -19,7 +18,7 @@ export default defineConfig(({ mode }) => {
       ],
     },
     define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL || 'http://localhost:8000/api'),
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8000/api'),
     },
   };
 });
